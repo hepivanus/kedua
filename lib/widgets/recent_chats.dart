2ea89d7a04cf6ed3972a5651f180894dc927b34c
hepivanus <hepivanus@gmail.com>
2021-08-25 02:50:36 -0700

    kedua

--- lib/widgets/recent_chats.dart
+++ lib/widgets/recent_chats.dart
@@ -1,21 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:kedua/models/message_models.dart';
 
-class RecentChats extends StatefulWidget {
-  @override
-  _RecentChatsState createState() => _RecentChatsState();
-}
-
-class _RecentChatsState extends State<RecentChats> {
+class RecentChats extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: Container(
         decoration: BoxDecoration(
-          color: Colors.white,
+          
           borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(30.0),
             topRight: Radius.circular(30.0),
+            topLeft: Radius.circular(30.0),
           ),
         ),
         child: ListView.builder(
@@ -23,16 +18,33 @@ class _RecentChatsState extends State<RecentChats> {
             itemBuilder: (BuildContext contaxt, int index) {
               final Message chat = chats[index];
               return Column(
-                children: <Widget> [
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  
                   CircleAvatar(
-                    radius: 25.0,
+                    radius: 35.0,
                     backgroundImage: AssetImage(chat.sender.imageUrl),
                   ),
-
+                  SizedBox(
+                    width: 10.0,
+                  ),
                   Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Text(chat.sender.name),
-
+                      Text(
+                        chat.sender.name,
+                        style: TextStyle(
+                            fontSize: 15.0,
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold),
+                      ),
+                       SizedBox(height: 5.0),
+                      Container(
+                        width: MediaQuery.of(context).size.width * 0.45,
+                        child: Text(chat.text, style: TextStyle(color: Colors.blueGrey, fontSize: 15.0, fontWeight: FontWeight.w600),
+                         overflow: TextOverflow.ellipsis,
+                        ),
+                      )
                     ],
                   ),
                 ],
